0afe1c8290eb779836f95586b3497b8289f1237f
Dustin Westaby <dwestaby@2g-eng.com>
2021-06-02 23:59:20 -0500

    Cleaned up with structs.  Dropped about 50 lines of code.

--- Arduino Code/streamDeckKeyboard/streamDeckKeyboard.ino
+++ Arduino Code/streamDeckKeyboard/streamDeckKeyboard.ino
@@ -11,71 +11,69 @@
 Encoder upDown(10,15);
 Encoder leftRight(14,16);
 
-char buttonArray1[] = {'a','b','c','d','e','s','g','h','i'}; //Array to save button presses
-char buttonArray2[] = {'j','k','l','m','n','o','p','q','r'}; // Add additional arrays for more pages
+typedef struct {
+    uint8_t pin;
+    uint8_t key;
+    uint8_t pinStateLast;
+} pinKey_t;
+
+#define NUM_KEYS 8
+
+pinKey_t pinKey1[NUM_KEYS] = {
+    { 2, 'a', HIGH }, //button r1.c2    
+    { 3, 'b', HIGH }, // PAGE SELECT    
+    { 4, 'c', HIGH }, //button r2.c1    
+    { 5, 'd', HIGH }, //button r2.c2    
+    { 6, 'e', HIGH }, //button r2.c3    
+    { 7, 'f', HIGH }, //knob 1          
+    { 8, 'g', HIGH }, //knob 2   
+    { 9, 'h', HIGH }, //button r1.c1           
+}; 
+
+pinKey_t pinKey2[NUM_KEYS] = {  
+    { 2, 'j', HIGH }, //button r1.c2    
+    { 3, 'k', HIGH }, // PAGE SELECT    
+    { 4, 'l', HIGH }, //button r2.c1    
+    { 5, 'm', HIGH }, //button r2.c2    
+    { 6, 'n', HIGH }, //button r2.c3    
+    { 7, 'o', HIGH }, //knob 1          
+    { 8, 'p', HIGH }, //knob 2   
+    { 9, 'q', HIGH }, //button r1.c1         
+}; 
 
 int pageFlag = 1; // Flag for button pages
 
-const int buttonPin2 = 2;
-const int buttonPin3 = 3;
-const int buttonPin4 = 4;
-const int buttonPin5 = 5;
-const int buttonPin6 = 6;
-const int buttonPin7 = 7;
-const int buttonPin8 = 8;
-const int buttonPin9 = 9;
-
-int pressedPin2 = HIGH;
-int pressedPin3 = HIGH;
-int pressedPin4 = HIGH;
-int pressedPin5 = HIGH;
-int pressedPin6 = HIGH;
-int pressedPin7 = HIGH;
-int pressedPin8 = HIGH; //Encoder Button 1 
-int pressedPin9 = HIGH; //Encoder Button 2
-
-int lastState2 = HIGH;
-int lastState3 = HIGH;
-int lastState4 = HIGH;
-int lastState5 = HIGH;
-int lastState6 = HIGH;
-int lastState7 = HIGH;
-int lastState8 = HIGH;
-int lastState9 = HIGH;
-
-unsigned long lastDebounceTime2 = 0; // Last time the button was pressed
-unsigned long lastDebounceTime3 = 0;
-unsigned long lastDebounceTime4 = 0;
-unsigned long lastDebounceTime5 = 0;
-unsigned long lastDebounceTime6 = 0;
-unsigned long lastDebounceTime7 = 0;
-unsigned long lastDebounceTime8 = 0;
-unsigned long lastDebounceTime9 = 0;
-
-long posUpDown = 0; //Position counter for up/down encoder
-long posLeftRight = 0; //Position counter for left/right encoder
-long posUpDownPrev = 0; //Previous position of up/down encoder
+long posUpDown        = 0; //Position counter for up/down encoder
+long posLeftRight     = 0; //Position counter for left/right encoder
+long posUpDownPrev    = 0; //Previous position of up/down encoder
 long posLeftRightPrev = 0; //Previous position of left/right encoder
 
 int counterUpDown = 0;
 int counterLeftRight = 0;
 
+bool pressed_flag = false;
+
 void setup() 
 {
+  Serial.begin(115200);
+  while (!Serial);
+  
   Serial.print("Initializing Pins...");
-  pinMode(buttonPin2, INPUT_PULLUP);
-  pinMode(buttonPin3, INPUT_PULLUP);
-  pinMode(buttonPin4, INPUT_PULLUP);
-  pinMode(buttonPin5, INPUT_PULLUP);
-  pinMode(buttonPin6, INPUT_PULLUP);
-  pinMode(buttonPin7, INPUT_PULLUP);
-  pinMode(buttonPin8, INPUT_PULLUP);
-  pinMode(buttonPin9, INPUT_PULLUP);
+
+  // Initialize pins
+  for (uint8_t i = 0; i < NUM_KEYS; i++) 
+  {
+      // Set pin to input
+      pinMode(pinKey1[i].pin, INPUT_PULLUP);
+
+      // Set current pin state
+      pinKey1[i].pinStateLast = digitalRead(pinKey1[i].pin);
+  }
   
   Serial.println(" Done!");
 
   Serial.print("Initializing Keyboard...");
-  Keyboard.begin();
+  //Keyboard.begin();
   Serial.println(" Done!");
 
   delay(50);
@@ -87,215 +85,163 @@ void setup()
 
 void loop() 
 {
+  process_buttons();
+  process_encoders();
+
+  if(pressed_flag)
+  {
+    //delay(50);
+    Keyboard.releaseAll(); // Release all pressed keys
+  }
+}
+
+/* ************************************ */
+/*               BUTTONS                */
+/* ************************************ */
+
+void process_buttons()
+{
+    uint8_t pinState;
+    uint8_t pinsState = 0;
+    
+    // BUTTONS
+    for (uint8_t i = 0; i < NUM_KEYS; i++) 
+    {
+        pinState = digitalRead(pinKey1[i].pin);
+        pinsState |= pinState << i;
+        
+        // Check if pin changed since last read
+        if (pinKey1[i].pinStateLast != pinState) 
+        {
+            Serial.print("Button Changed: ");
+            Serial.println(i);
   
-  pressedPin2 = digitalRead(2);
-  pressedPin3 = digitalRead(3);
-  pressedPin4 = digitalRead(4);
-  pressedPin5 = digitalRead(5);
-  pressedPin6 = digitalRead(6);
-  pressedPin7 = digitalRead(7);
-  pressedPin8 = digitalRead(8);
-  pressedPin9 = digitalRead(9);
+            if(pinState == LOW)
+            {
+              Serial.print("Button Pressed: ");
+              Serial.println(i);
+              //button was "just" pressed
+              switch (i)
+              {
+                case 0: // pin 2
+                case 2: // pin 4
+                case 3: // pin 5
+                case 4: // pin 6
+                case 5: // pin 7
+                case 6: // pin 8
+                case 7: // pin 9
+                  press_key_from_list(i);
+                  break;
+                case 1: // pin 3
+                  change_page();
+                  break;
+                default:
+                  break;
+              }
+              
+            } //end button just pressed
+        } //end key change
+        pinKey1[i].pinStateLast = pinState;
+    } //end loop
 
- // BUTTONS
- if (pressedPin2 != lastState2)
-  {
-    if (pressedPin2 == LOW)
+    //Serial.print("Button Readings: ");
+    //Serial.println(pinsState, BIN);
+}
+
+void press_key_from_list(byte i)
+{
+    switch (pageFlag) //Depending on pageFlag, send different keystroke
     {
-      switch (pageFlag) //Depending on pageFlag, send different keystroke
+      case 1:
+        Keyboard.press(pinKey1[i].key);
+        break;
+      default:
+        Keyboard.press(pinKey2[i].key);
+        break;
+    }
+    pressed_flag = true;
+    Serial.print("Key Pressed");
+}
+
+void change_page()
+{
+    if (pageFlag == 1)
+    {
+      pageFlag = 2;
+      digitalWrite(A0, HIGH);
+    }
+    else //if (pageFlag == 2)
+    {
+      pageFlag = 1;
+      digitalWrite(A0, LOW);
+    }
+    Serial.print("Page Flag: ");
+    Serial.println(pageFlag);
+}
+
+/* ************************************ */
+/*              ENCODERS                */
+/* ************************************ */
+
+void process_encoders()
+{
+    // ENCODERS
+    posUpDown = upDown.read(); //Up/down encoder
+    if (posUpDown!=posUpDownPrev) //If new position doesn't equal previous position
+    {
+      if ((posUpDown-posUpDownPrev)>0)
       {
-        case 1:
-          Keyboard.press(buttonArray1[0]); // Send keystroke
-          break;
-        default:
-          Keyboard.press(buttonArray2[0]);
-          break;
+        counterUpDown = counterUpDown+1;
+        if (counterUpDown>3) // 3 counts per physical encoder "click"
+        {
+          Keyboard.press(KEY_UP_ARROW);
+          pressed_flag = true;
+          counterUpDown = 0;
+        }
+      }
+      else if ((posUpDown-posUpDownPrev)<0)
+      {
+        counterUpDown = counterUpDown-1;
+        if (counterUpDown<-3)
+        {
+          Keyboard.press(KEY_DOWN_ARROW);
+          counterUpDown = 0;
+          pressed_flag = true;
+        }
       }
     }
-  }
-
-  if (pressedPin3 != lastState3) // Button 3 assigned to page select
-  {
-    if (pressedPin3 == LOW)
+    if(pressed_flag)
     {
-      if (pageFlag == 1)
-      {
-        pageFlag = 2;
-        digitalWrite(A0, HIGH);
-      }
-      else if (pageFlag == 2)
-      {
-        pageFlag = 1;
-        digitalWrite(A0, LOW);
-      }
+      //delay(50);
+      Keyboard.releaseAll();
     }
-  }
-  Serial.print("Page Flag: ");
-  Serial.println(pageFlag);
-
-  if (pressedPin4 != lastState4)
-  {
-    if (pressedPin4 == LOW)
-    {
-      switch (pageFlag) //Depending on pageFlag, send different keystroke
-      {
-        case 1:
-          Keyboard.press(buttonArray1[2]); // Send keystroke
-          break;
-        default:
-          Keyboard.press(buttonArray2[2]);
-          break;
-      }
-    }
-  }
-
-  if (pressedPin5 != lastState5)
-  {
-    if (pressedPin5 == LOW)
-    {
-      switch (pageFlag) //Depending on pageFlag, send different keystroke
-      {
-        case 1:
-          Keyboard.press(buttonArray1[3]); // Send keystroke
-          break;
-        default:
-          Keyboard.press(buttonArray2[3]);
-          break;
-      }
-    }
-  }
-
-  if (pressedPin6 != lastState6)
-  {
-    if (pressedPin6 == LOW)
-    {
-      switch (pageFlag) //Depending on pageFlag, send different keystroke
-      {
-        case 1:
-          Keyboard.press(buttonArray1[4]); // Send keystroke
-          break;
-        default:
-          Keyboard.press(buttonArray2[4]);
-          break;
-      }
-    }
-  }
-
-  if (pressedPin7 != lastState7)
-  {
-    if (pressedPin7 == LOW)
-    {
-      switch (pageFlag) //Depending on pageFlag, send different keystroke
-      {
-        case 1:
-          Keyboard.press(buttonArray1[5]); // Send keystroke
-          break;
-        default:
-          Keyboard.press(buttonArray2[5]);
-          break;
-      }
-    }
-  }
-
-  if (pressedPin8 != lastState8)
-  {
-    if (pressedPin8 == LOW)
-    {
-      switch (pageFlag) //Depending on pageFlag, send different keystroke
-      {
-        case 1:
-          Keyboard.press(buttonArray1[6]); // Send keystroke
-          break;
-        default:
-          Keyboard.press(buttonArray2[6]);
-          break;
-      }
-    }
-  }
-
-  if (pressedPin9 != lastState9)
-  {
-    if (pressedPin9 == LOW)
-    {
-      switch (pageFlag) //Depending on pageFlag, send different keystroke
-      {
-        case 1:
-          Keyboard.press(buttonArray1[7]); // Send keystroke
-          break;
-        default:
-          Keyboard.press(buttonArray2[7]);
-          break;
-      } 
-    }
-  }
-
-  // ENCODERS
-  posUpDown = upDown.read(); //Up/down encoder
-  if (posUpDown!=posUpDownPrev) //If new position doesn't equal previous position
-  {
-    if ((posUpDown-posUpDownPrev)>0)
-    {
-      counterUpDown = counterUpDown+1;
-      if (counterUpDown>3) // 3 counts per physical encoder "click"
-      {
-        Keyboard.press(KEY_UP_ARROW);
-        counterUpDown = 0;
-        Keyboard.releaseAll();
-      }
-    }
-    else if ((posUpDown-posUpDownPrev)<0)
-    {
-      counterUpDown = counterUpDown-1;
-      if (counterUpDown<-3)
-      {
-        Keyboard.press(KEY_DOWN_ARROW);
-        counterUpDown = 0;
-        Keyboard.releaseAll();
-      }
-    }
-  }
-  Keyboard.releaseAll();
-  posUpDownPrev = posUpDown;
-  //Serial.println(counterUpDown);
-
-  posLeftRight = leftRight.read(); // Left/right encoder
-  if (posLeftRight!=posLeftRightPrev) //If new position doesn't equal previous position
-  {
-    if ((posLeftRight-posLeftRightPrev)>0)
-    {
-      counterLeftRight = counterLeftRight+1;
-      if (counterLeftRight>3) // 3 counts per physical encoder "click"
-      {
-        Keyboard.press(KEY_RIGHT_ARROW);
-        counterLeftRight = 0;
-        Keyboard.releaseAll();
-      }
-    }
-    else if ((posLeftRight-posLeftRightPrev)<0)
-    {
-      counterLeftRight = counterLeftRight-1;
-      if (counterLeftRight<-3)
-      {
-        Keyboard.press(KEY_LEFT_ARROW);
-        counterLeftRight = 0;
-        Keyboard.releaseAll();
-      }
-    }
-  }
-  Keyboard.releaseAll();
-  posLeftRightPrev = posLeftRight;
-  Serial.println(counterLeftRight);
-
-
-  // BUTTON STATE MANAGEMENT
-  lastState2 = pressedPin2; // Save the latest reading
-  lastState3 = pressedPin3;
-  lastState4 = pressedPin4;
-  lastState5 = pressedPin5;
-  lastState6 = pressedPin6;
-  lastState7 = pressedPin7;
-  lastState8 = pressedPin8;
-  lastState9 = pressedPin9;
+    posUpDownPrev = posUpDown;
+    //Serial.println(counterUpDown);
   
-  Keyboard.releaseAll(); // Release all pressed keys
+    posLeftRight = leftRight.read(); // Left/right encoder
+    if (posLeftRight!=posLeftRightPrev) //If new position doesn't equal previous position
+    {
+      if ((posLeftRight-posLeftRightPrev)>0)
+      {
+        counterLeftRight = counterLeftRight+1;
+        if (counterLeftRight>3) // 3 counts per physical encoder "click"
+        {
+          Keyboard.press(KEY_RIGHT_ARROW);
+          counterLeftRight = 0;
+          pressed_flag = true;
+        }
+      }
+      else if ((posLeftRight-posLeftRightPrev)<0)
+      {
+        counterLeftRight = counterLeftRight-1;
+        if (counterLeftRight<-3)
+        {
+          Keyboard.press(KEY_LEFT_ARROW);
+          counterLeftRight = 0;
+          pressed_flag = true;
+        }
+      }
+    }
+    posLeftRightPrev = posLeftRight;
+    //Serial.println(counterLeftRight);
 }
